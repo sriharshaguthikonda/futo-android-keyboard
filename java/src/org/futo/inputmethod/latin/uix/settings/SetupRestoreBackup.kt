26630cb75827eae9b8ca874f97be49929330223c
Dr. Sri Harsha Guthikonda <g.sriharsha746electronics@gmail.com>
2025-07-11 22:25:44 +0530

    Remove duplicate preference keys and fix SetupRestoreBackup

--- java/src/org/futo/inputmethod/latin/uix/settings/SetupRestoreBackup.kt
+++ java/src/org/futo/inputmethod/latin/uix/settings/SetupRestoreBackup.kt
@@ -4,8 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
