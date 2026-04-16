175734d6a84c7d552acbd1c7681dc8a7d4a32948
Aleksandras Kostarevas <alex@futo.org>
2026-04-16 19:59:00 +0300

    Force recreate UserBinaryDictionaries on service restart
    We can't be sure that the user dictionary hasn't been modified in the time that our service wasn't running

--- java/src/org/futo/inputmethod/latin/LatinIME.kt
+++ java/src/org/futo/inputmethod/latin/LatinIME.kt
@@ -368,6 +368,8 @@ class LatinIME : InputMethodServiceCompose(), LatinIMELegacy.SuggestionStripCont
     override fun onCreate() {
         super.onCreate()
 
+        UserBinaryDictionary.resetUserDictionariesRequiringRecreation()
+
         CanThrowIfDebug = isDirectBootUnlocked
 
         JniUtils.loadNativeLibrary()
@@ -884,6 +886,7 @@ class LatinIME : InputMethodServiceCompose(), LatinIMELegacy.SuggestionStripCont
         Log.i("LatinIME", "Device has been unlocked, reloading settings")
 
         // Every place that called getDefaultSharedPreferences now needs to be refreshed or call it again
+        UserBinaryDictionary.resetUserDictionariesRequiringRecreation()
 
         // Mainly Settings singleton needs to be refreshed
         Settings.init(applicationContext)
